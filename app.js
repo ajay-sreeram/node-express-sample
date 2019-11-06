8837b8c35f6d6581fbbc83256444abde990e243a
ajay <sreeram.ajay18@gmail.com>
2019-11-06 19:27:01 +0530

    Access-Control-Allow-Origin two times

--- app.js
+++ app.js
@@ -248,6 +248,15 @@ app.use((err, req, res, next) => {
 
         res.status(500).json({message:err.message}) 
     } 
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 })
 
 
