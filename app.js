aec168c376e3e74ed87375d3b836209604614aa4
ajay <sreeram.ajay18@gmail.com>
2019-11-06 19:17:53 +0530

    added allow cross origin

--- app.js
+++ app.js
@@ -217,6 +217,26 @@ router.get('/download_audio/:audioname', (req, res) => {
 
 app.use('/', router)
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    //res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use((err, req, res, next) => {
 
     if (err.code == 'ENOENT') {
